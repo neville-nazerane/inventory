058d2cfd066490dda086c41d78d40db9e42ab5cc
Neville Nazerane <live@neville.com>
2024-12-19 22:38:36 -0700

    setup

---
+++ src/Inventory.ServerLogic/Utils/ServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.ServerLogic.Utils
+{
+    public static class ServiceExtensions
+    {
+
+        public static IServiceCollection AddAllServices(this IServiceCollection services)
+        {
+            services.AddScoped<UserService>();
+
+            return services;
+        }
+
+    }
+}
--- src/Inventory.WebAPI/Program.cs
+++ src/Inventory.WebAPI/Program.cs
@@ -1,11 +1,17 @@
 
+using Inventory.ServerLogic.Utils;
 using Inventory.WebAPI.Endpoints;
 using Inventory.WebAPI.Utils;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// services
+builder.Services.AddAllServices();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Endpoints
+app.MapGet("/", () => "Hello Inventory World!");
 
 app.HandleExceptions();
 
