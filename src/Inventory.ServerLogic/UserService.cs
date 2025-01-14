c29b38376ad6e6f764eeb1129c5b20af4c06a2b8
Neville Nazerane <live@neville.com>
2025-01-14 00:19:58 -0700

    cleanups

--- src/Inventory.ServerLogic/UserService.cs
+++ src/Inventory.ServerLogic/UserService.cs
@@ -31,10 +31,10 @@ namespace Inventory.ServerLogic
             }
         }
 
-        public async Task<string> GetJwtAsync(LoginModel login)
-        {
-            _manager.UserManager.GenerateUserTokenAsync
-        }
+        //public async Task<string> GetJwtAsync(LoginModel login)
+        //{
+        //    //_manager.UserManager.GenerateUserTokenAsync()
+        //}
 
     }
 }
