675b074b72c5d6325807e647f6493a452f27e32c
Neville Nazerane <live@neville.com>
2025-01-19 02:09:43 -0700

    finishing off auth setup

--- src/Inventory.Website/Program.cs
+++ src/Inventory.Website/Program.cs
@@ -12,11 +12,14 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new AuthClient(new(new AuthApiHandler())
 {
-    BaseAddress = new("https://auth.nevillenazerane.com")
+    BaseAddress = new("http://localhost:5043")
 }));
+    //BaseAddress = new("https://auth.nevillenazerane.com")
 
 builder.Services.AddSingleton<AuthenticationManager>()
                 .AddSingleton<AuthenticationStateProvider>(sp => sp.GetService<AuthenticationManager>() ?? throw new Exception("Can't get auth manager"));
+
+builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
 
